a8f9847dbba80a029b98e2b66c498219b02260ab
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-28 15:46:38 -0400

    eh

--- testing/ember-app/tests/utils/service/js-test.ts
+++ testing/ember-app/tests/utils/service/js-test.ts
@@ -1,11 +1,11 @@
 import { tracked } from '@glimmer/tracking';
 import { setOwner } from '@ember/application';
+import { destroy, registerDestructor } from '@ember/destroyable';
+import { settled } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
 import { service } from 'ember-resources/util/service';
-import { destroy, registerDestructor } from '@ember/destroyable';
-import { settled } from '@ember/test-helpers';
 
 module('Utils | service | js', function (hooks) {
   setupTest(hooks);
@@ -53,7 +53,6 @@ module('Utils | service | js', function (hooks) {
       setOwner(myTest, owner);
       destroy(owner);
 
-
       // property on service is accessed, so it is created
       owner = {};
       myTest = new Test();
@@ -63,6 +62,63 @@ module('Utils | service | js', function (hooks) {
       await settled();
 
       assert.verifySteps(['created', 'destroying']);
+    });
+
+    test('state is shared between injections (when the owner is the same)', async function (assert) {
+      class MyService {
+        @tracked data = 0;
+
+        inc = () => this.data++;
+      }
+
+      class Test1 {
+        @service(MyService) declare foo: MyService;
+      }
+      class Test2 {
+        @service(MyService) declare foo: MyService;
+      }
+
+      let myTest1 = new Test1();
+      let myTest2 = new Test2();
+
+      setOwner(myTest1, this.owner);
+      setOwner(myTest2, this.owner);
+
+      assert.strictEqual(myTest1.foo.data, 0);
+
+      myTest1.foo.inc();
+
+      assert.strictEqual(myTest2.foo.data, 1);
+    });
+
+    test('state is NOT shared between injections (when the owner is different)', async function (assert) {
+      class MyService {
+        @tracked data = 0;
+
+        inc = () => this.data++;
+      }
+
+      let owner1 = this.owner;
+      class Test1 {
+        @service(MyService) declare foo: MyService;
+      }
+      let owner2 = {};
+      class Test2 {
+        @service(MyService) declare foo: MyService;
+      }
+
+      let myTest1 = new Test1();
+      let myTest2 = new Test2();
+
+      setOwner(myTest1, owner1);
+      setOwner(myTest2, owner2);
+
+      assert.strictEqual(myTest1.foo.data, 0);
+
+      myTest1.foo.inc();
+
+      assert.strictEqual(myTest1.foo.data, 1);
+      assert.strictEqual(myTest2.foo.data, 0);
     });
   });
 });
